e9f68266be0e8cb557dd99002fb44c52211aad1a
Jomar <jomarsoliman01@gmail.com>
2025-07-26 13:56:33 +0800

    Correct speliing of paid

--- database/migrations/2025_07_26_053126_create_invoices_table.php
+++ database/migrations/2025_07_26_053126_create_invoices_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->integer('amount');
             $table->string('status'); // B = Builed, P = Paid, V = Void
             $table->dateTime('builed_date');
-            $table->dateTime('paied_date')->nullable();
+            $table->dateTime('paid_date')->nullable();
             $table->timestamps();
         });
     }
