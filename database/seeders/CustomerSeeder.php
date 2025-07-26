dad9ea9846136d6b9e11c9edc1b49e8cfa104c8f
Jomar <jomarsoliman01@gmail.com>
2025-07-26 14:11:56 +0800

    Create a different variation of data that will make a factory

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +14,28 @@ class CustomerSeeder extends Seeder
     public function run(): void
     {
         //
+
+        //25 Customer with 10 Invoices each
+        Customer::factory()
+                ->count(25)
+                ->hasInvoices(10)
+                ->create();
+
+        //100 Customer with 5 Invoices each
+        Customer::factory()
+                ->count(100)
+                ->hasInvoices(5)
+                ->create();
+
+        //100 Customer with 3 Invoices each
+        Customer::factory()
+                ->count(100)
+                ->hasInvoices(3)
+                ->create();
+
+        //100 Customer without any Invoice
+        Customer::factory()
+                ->count(100)
+                ->create();
     }
 }
