26257bb5d6b23ef53727341160476ae02162a8a2
Jomar <jomarsoliman01@gmail.com>
2025-07-26 15:15:28 +0800

    Add some data structure

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -6,6 +6,7 @@ use App\Models\Customer;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 
 class CustomerController extends Controller
@@ -16,7 +17,7 @@ class CustomerController extends Controller
     public function index()
     {
         //
-        return Customer::all();
+        return new CustomerCollection(Customer::all());
     }
 
     /**
--- app/Http/Resources/V1/CustomerResource.php
+++ app/Http/Resources/V1/CustomerResource.php
@@ -14,14 +14,14 @@ class CustomerResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'id'    =>  $this->id,
-            'name'    =>  $this->name,
-            'email'    =>  $this->email,
-            'address'    =>  $this->address,
-            'city'    =>  $this->city,
-            'state'    =>  $this->state,
-            'postalCode'    =>  $this->postal_code,
+        return[
+            'id'    => $this->id,
+            'name'    => $this->name,
+            'email'    => $this->email,
+            'address'    => $this->address,
+            'city'    => $this->city,
+            'state'    => $this->state,
+            'postalCode'    => $this->postal_code,
         ];
     }
 }
