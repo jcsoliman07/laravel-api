af06975dc7589b36d35f0c43e8e0e6b44100ecca
Jomar <jomarsoliman01@gmail.com>
2025-07-26 15:24:41 +0800

    Access invoice resource and collection to fetch data

--- app/Http/Controllers/Api/InvoiceController.php
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -6,6 +6,8 @@ use App\Models\Invoice;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\InvoiceCollection;
+use App\Http\Resources\V1\InvoiceResource;
 
 class InvoiceController extends Controller
 {
@@ -15,6 +17,7 @@ class InvoiceController extends Controller
     public function index()
     {
         //
+        return new InvoiceCollection(Invoice::paginate(10));
     }
 
     /**
@@ -39,6 +42,7 @@ class InvoiceController extends Controller
     public function show(Invoice $invoice)
     {
         //
+        return new InvoiceResource($invoice);
     }
 
     /**
